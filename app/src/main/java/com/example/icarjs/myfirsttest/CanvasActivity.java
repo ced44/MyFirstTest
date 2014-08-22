fc6bbae7d09808bd616d85e4ea96f95d99fa06c6
cedric Prudent <prudent.ced@gmail.com>
2014-08-22 16:15:05 +0200

    mise en place des threads + son

--- app/src/main/java/com/example/icarjs/myfirsttest/CanvasActivity.java
+++ app/src/main/java/com/example/icarjs/myfirsttest/CanvasActivity.java
@@ -8,6 +8,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,8 +22,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Random;
 
 
@@ -35,6 +40,12 @@ public class CanvasActivity extends Activity {
     DisplayView realDisplay = null;
     Circle circle = null;
     Random random = new Random();
+    private SoundPool soundPool;
+    private HashMap<Integer, Integer> soundsMap;
+    int SOUND1=1;
+    int SOUND2=2;
+    private String url ="https://ia700600.us.archive.org/14/items/TetrisThemeMusic/Tetris.ogg";
+    private MediaPlayer mp = new MediaPlayer();
 
 // ----------------------- le GameOver servira de flag pour annoncer la fin de la partie -----------
     public boolean getGameOver(){
@@ -48,23 +59,25 @@ public class CanvasActivity extends Activity {
         @Override
         public void run() {
             if(!getGameOver()) {
-                mHandler.postDelayed(monThread, 4000);
+                mHandler.postDelayed(monThread, 5000);
 
                 System.out.println("Mon Thread ressemble à ça  ===> ");
                 System.out.println("Mon Thread ressemble à ça  ===> "+monThread.toString());
-                realDisplay.postInvalidate();// permet le rafraichissement de la view par invalidation
+                realDisplay.invalidate();// permet le rafraichissement de la view par invalidation
 
                 System.out.println(" ------------------je suis passé par monThread !! --------------------");
                 runDrawing(); // 1 cercle par thread
 
             }
             else {
+                mHandler.removeCallbacks(monThread);
                 realDisplay.circles.clear();
                 Context context = getApplicationContext();
                 CharSequence text = "GAME OVER !!!!! ";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
+                realDisplay.postInvalidate();
             }
 
         }
@@ -113,31 +126,51 @@ public class CanvasActivity extends Activity {
     public void runDrawing(){
 
         circle = new Circle();
-        int x = DisplayView.getInstance(DisplayView.context).getWidth();//1600;//findViewById(R.id.canvas).getWidth();
-        int y = DisplayView.getInstance(DisplayView.context).getHeight();//800;//findViewById(R.id.canvas).getHeight();
+        int x = realDisplay.getWidth()-200;//1600;//findViewById(R.id.canvas).getWidth();
+        int y = realDisplay.getHeight()-200;//800;//findViewById(R.id.canvas).getHeight();
         System.out.println("largeur d'écran  =================> "+x);
         System.out.println("longueur d'écran  =================> "+y);
         if (x>0 || y>0) {
-            circle.centerX = random.nextInt(x) - 80;// permet d'obtenir un nombre aléatoire entre
-            circle.centerY = random.nextInt(y) - 80;// 50 et le nombre passer en paramètre
-        } else {
+                do{
+                    circle.centerX =  ((float)Math.random()*x);// permet d'obtenir un nombre aléatoire entre
+                    circle.centerY =  ((float)Math.random()*y);// 50 et le nombre passer en paramètre
+                    System.out.println("placement cerle en X  =================> "+circle.centerX);
+                    System.out.println("placement cerle en Y  =================> "+circle.centerY);
+                }
+                while (findCircle2(circle.centerX, circle.centerY));
+
+        }
+        else {
             Log.e("num error", "les coordonnées du cercle sont négatives");
         }
-        circle.radius = 50 + 35 * random.nextFloat();// la taille du cercle devra dépendre de la montée du score
-        circle.fillColor = 0xff000000;// affectation de la couleur
-        realDisplay.circles.add(circle);// ajout du cercle a la collection
-        System.out.println("un cercle a été créer !!!!!");
-        realDisplay.postInvalidate();// permet le rafraichissement de la view par invalidation
 
-        //mct.doInBackground();
-        Thread2.run();
-        //mHandler3.postDelayed(Thread2, 1000);
-
-
-
-
+                    circle.radius = 50 + 35* random.nextFloat();// la taille du cercle devra dépendre de la montée du score
+                    circle.fillColor = 0xff000000;// affectation de la couleur
+                    realDisplay.circles.add(circle);// ajout du cercle a la collection
+                    System.out.println("un cercle a été créer !!!!!");
+                    realDisplay.postInvalidate();// permet le rafraichissement de la view par invalidation
+                    //Thread2.run();
+                   mHandler3.postDelayed(Thread2, 1000);
 
     }
+
+
+    public boolean findCircle2(float x, float y) {
+
+        Circle found = null;
+        for (Circle circle : realDisplay.circles) {
+            if (circle.contains(x, y)) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
 
     // -------------- Fonction pour reduire progressivement la taille du cercle
     public void modifCircle(){
@@ -182,7 +215,8 @@ public class CanvasActivity extends Activity {
         //System.out.println("information sur la longueur de l'écran  : "+Display.getInstance(Display.context).getWidth());
         //System.out.println("information sur la largeur de l'écran  : "+Display.getInstance(Display.context).getHeight());
         // blocage de l'écran en mode paysage
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getActionBar().hide();
         realDisplay = DisplayView.getInstance(this);//Display.getInstance(this);//new Display(this);
         realDisplay.init();
         ViewGroup parent = (ViewGroup) realDisplay.getParent();
@@ -190,11 +224,48 @@ public class CanvasActivity extends Activity {
             parent.removeAllViews();
         }
         setContentView(realDisplay);
+        soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 100);
+        soundsMap = new HashMap<Integer, Integer>();
+        soundsMap.put(SOUND1, soundPool.load(this, R.raw.Tetris, 1));
+        System.out.println("-------------------------" + soundsMap.get(1) + "------------------------");
+        //soundsMap.put(SOUND2, soundPool.load(this, R.raw.mafia2, 1));
         //mHandler.postDelayed(monThread, 1000);// déclenchement des thread
         System.out.println(" //////////////////  RUN thread RUUUNNNNNN !!!! //////////////////////");
 
         monThread.run();
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        /*
+        try {
+            mp.setDataSource(url);
+            //mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        */
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            public void onPrepared(MediaPlayer mp) {
+                playSound(SOUND1, 1);
+            }
+        });
+        mp.prepareAsync();
+
+        //playSound(SOUND1, 1);
     }
+
+
+
+    public void playSound(int sound, float fSpeed) {
+        AudioManager mgr = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = streamVolumeCurrent / streamVolumeMax;
+
+        soundPool.play(soundsMap.get(sound), volume, volume, 1, 0, fSpeed);
+    }
+
+
+
 
 
 }
